969fa69f7416505ebec7838c610708477dd0de29
maroodb <marouen.dbouba@gmail.com>
2022-03-19 22:23:34 +0100

    setup page structure

---
+++ src/config/pages.config.js
@@ -0,0 +1,13 @@
+export const pages = [
+
+    {
+        title: 'SimpleEvent is ready!',
+        url: '#simpleEvent.html'
+    },
+    {
+        title: 'This is a test',
+        url: '#test.html'
+    }
+
+]
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import _ from 'lodash';
+import { pages } from "./config/pages.config";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const main = () => {
+    console.log('create sidenav..');
+    setSideNavMenu();
+};
+
+
+function load_home() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="./dist/pages/simpleEvent.html" ></object>';
+}
+
+
+const setSideNavMenu = () => {
+    console.log("set side");
+    const menu = getSideNavMenuElement();
+    console.log(menu);
+    console.log(pages);
+    for (const page of pages) {
+        console.log(page);
+        const menuItem = createMenuItemFrom(page);
+        menu.appendChild(menuItem);   
+    }
+}
+
+const getSideNavMenuElement = () => {
+    return document.getElementById("sidenav");
+}
+
+const createMenuItemFrom = (page) => {
+    var item = document.createElement("a");
+    var linkText = document.createTextNode(page.title);
+    item.appendChild(linkText);
+    item.href = page.url;
+    return item;
+}
+
+
+main();
